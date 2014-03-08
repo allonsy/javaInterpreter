01cd3cee8117bec42412ee3317e8985472c41f5d
Alec S <linuxbash8@gmail.com>
2014-03-08 00:43:08 -0600

    first commit

---
+++ JSH.java
@@ -0,0 +1,122 @@
+import edu.rice.cs.drjava.model.repl.newjvm.ClassPathManager;
+import edu.rice.cs.plt.text.TextUtil;
+import edu.rice.cs.plt.reflect.ReflectUtil;
+import edu.rice.cs.plt.tuple.Option;
+import edu.rice.cs.dynamicjava.Options;
+import edu.rice.cs.dynamicjava.interpreter.Interpreter;
+import edu.rice.cs.dynamicjava.interpreter.InterpreterException;
+import edu.rice.cs.dynamicjava.interpreter.EvaluatorException;
+import edu.rice.cs.drjava.model.repl.InteractionsPaneOptions;
+public class JSH
+{
+    ClassPathManager _classPathManager;
+    InteractionsPaneOptions _interpreterOptions;
+    Interpreter _interpreter;
+    boolean fileFlag;
+    public JSH()
+    {
+        _classPathManager=new ClassPathManager(ReflectUtil.SYSTEM_CLASS_PATH);
+        _interpreterOptions=new InteractionsPaneOptions();
+        _interpreter=new Interpreter(_interpreterOptions, _classPathManager.makeClassLoader(null));
+        fileFlag=false;
+    }
+	public void _interpret(String toEval) {
+	try {
+      Option<Object> result = this._interpreter.interpret(toEval);
+      if (result.isSome()) {
+        String objString = null;
+        try { 
+                objString = TextUtil.toString(result.unwrap());
+                if(!fileFlag)
+                {
+                    System.out.println(objString);
+                }
+            }
+        catch (Throwable t) { throw new EvaluatorException(t); }
+      }
+    }
+    catch (InterpreterException e) {
+      System.out.println("Error");
+    }
+  }
+  public void interMethod(String in, EasyReader console)
+  {
+      int braceCount=0;
+      boolean quote=false;
+      for(int i=0; i<in.length(); i++)
+      {
+          if(in.charAt(i)=='"' && i==0)
+          {
+              quote=!quote;
+          }
+          else if((in.charAt(i)=='"')&&(!(in.charAt(i-1)=='\\'))&&(!(in.charAt(i-1)=='\'')))
+          {
+              quote=!quote;
+          }
+          else if(in.charAt(i)=='{' && !quote)
+          {
+              braceCount++;
+          }
+          else if(in.charAt(i)=='}' && !quote)
+          {
+              braceCount--;
+          }
+      }
+      if(braceCount==0)
+      {
+        _interpret(in);
+        System.out.print("> ");
+      }
+      else
+      {
+          in=in+"\n"+console.readLine();
+          interMethod(in, console);
+      }
+  }
+  public void parseFile(String [] args)
+  {
+      fileFlag=true;
+      for(int i=0; i<args.length; i++)
+      {
+          EasyReader reader=new EasyReader(args[i]);
+          String f="";
+          f=reader.readLine();
+          while(!reader.eof())
+          {
+              f=f+"\n"+reader.readLine();
+          }
+          _interpret(f);
+          System.out.println("Succesfully import file: "+args[i]);
+      }
+      fileFlag=false;
+  }
+  public static void main(String[] args)
+  {
+      JSH inter=new JSH();
+      EasyReader console=new EasyReader();
+      System.out.println("Welcome to the command line java interpreter!");
+      System.out.println("Please call System.exit(0), or CTRL-D to quit");
+      if(args.length>0)
+      {
+          inter.parseFile(args);
+      }
+      String input;
+      System.out.print("> ");
+      input=console.readLine();
+      while(!console.eof())
+      {
+        if(input.contains("{"))
+        {
+            inter.interMethod(input, console);
+            input=console.readLine();
+        }
+        else
+        {
+            inter._interpret(input);
+            System.out.print("> ");
+            input=console.readLine();
+        }
+      }
+      System.exit(0);
+  }
+}
---
+++ jsh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -classpath ".:drjava.jar" JSH $@
